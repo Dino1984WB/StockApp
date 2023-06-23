ac6f89fa5324371ed4f8515c61f38a7291610c86
Dino1984WB <williamkbukowkidev@gmail.com>
2023-06-23 16:21:45 -0400

    addeed js

--- stock_alerts.js
+++ stock_alerts.js
@@ -1,23 +1,35 @@
-function sendAlert(phone_number, stock_ticker, threshold) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "send_alert.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({
-      phone_number: phone_number,
-      stock_ticker: stock_ticker,
-      threshold: threshold
-    }));
+async function sendAlert(phone_number, stock_ticker, threshold) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "send_alert.php", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  await xhr.send(JSON.stringify({
+    phone_number: phone_number,
+    stock_ticker: stock_ticker,
+    threshold: threshold
+  }));
+}
+
+const apiKey = process.env.YAHOO_FINANCE_API_KEY;
+
+async function onSubmit() {
+  var phone_number = $("#phone_number").val();
+  var stock_ticker = $("#stock_ticker").val();
+  var threshold = $("#threshold").val();
+
+  if (phone_number && stock_ticker && threshold) {
+    const url = `https://finance.yahoo.com/quote/?symbols=${stock_ticker}&format=json&region=US&lang=en-US&apikey=${apiKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const price = data.quoteSummary.price;
+
+    if (price > threshold) {
+      await sendAlert(phone_number, stock_ticker, threshold);
+    }
   }
-  
-  $(document).ready(function() {
-    $("#submit").click(function() {
-      var phone_number = $("#phone_number").val();
-      var stock_ticker = $("#stock_ticker").val();
-      var threshold = $("#threshold").val();
-  
-      if (phone_number && stock_ticker && threshold) {
-        sendAlert(phone_number, stock_ticker, threshold);
-      }
-    });
-  });
-  
+}
+
+$(document).ready(function() {
+  $("#submit").click(onSubmit);
+});
